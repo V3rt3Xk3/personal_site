a791d36e5266037fcacb343211e825a081ff294e
V3rt3Xk3 <zoltanbalazsacademic@gmail.com>
2021-03-22 23:26:09 +0100

    //i am in the process of learning datatypes in TypeScript
    Practically just ignore this commit

--- index.ts
+++ index.ts
@@ -1,8 +1,33 @@
-console.log("Helo");
+type string_or_number = string | number;
 
-let num: number = 1;
-let num1: number = 3;
+function combine(
+    input1: string_or_number, 
+    input2: string_or_number, 
+    resultType: 'as-number' | 'as-text',
+): string_or_number {
+	let result: number | string;
 
-function test_func(num: number) {
-	console.log(num);
+	if (typeof input1 === "number" && typeof input2 === "number" || resultType === "as-number") {
+		result = +input1 + +input2;
+	} else {
+		result = input1.toString() + input2.toString();
+	}
+
+	return result;
 }
+
+const combinedAges = combine(30, 26, 'as-number');
+console.log(combinedAges);
+
+const combinedAgesStringsAsNum = combine('30', '26', 'as-number');
+console.log(combinedAges);
+
+const combinedNames = combine("Max", "Anna", 'as-text');
+console.log(combinedNames);
+
+function printToConsole():void{
+    console.log("Printing...");
+}
+printToConsole();
+
+
