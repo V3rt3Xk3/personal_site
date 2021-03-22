c4f0ee5a81346e029ba2dbddc6bc01ff37077cf0
V3rt3Xk3 <zoltanbalazsacademic@gmail.com>
2021-03-22 21:36:37 +0100

    // i have initialized the project with $npm init

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log("Helo");
+var num = 1;
+var num1 = 3;
+function test_func(num) {
+    console.log(num);
+}
---
+++ index.ts
@@ -0,0 +1,8 @@
+console.log("Helo");
+
+let num: number = 1;
+let num1: number = 3;
+
+function test_func(num: number) {
+	console.log(num);
+}
