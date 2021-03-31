30d1c225a0a09ee7620f31231f396bbefc14524e
V3rt3Xk3 <zoltanbalazsacademic@gmail.com>
2021-03-31 22:04:58 +0200

    // some listening and porting

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello world");
+import express from "express";
+
+const app = express();
+
+// Middlewares
+
+// Routes
+app.get("/", (_request, _response) => {
+	_response.send("We are on home");
+});
+
+// Listening for a port
+app.listen(3000);
