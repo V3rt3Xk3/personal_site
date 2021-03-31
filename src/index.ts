158bf52b63e7df15bd5f1fa7e2f64eb4151276b3
V3rt3Xk3 <zoltanbalazsacademic@gmail.com>
2021-03-31 23:13:28 +0200

    // prettified a bit the connection part

--- src/index.ts
+++ src/index.ts
@@ -12,23 +12,23 @@ app.get("/", (_request, _response) => {
 // Listening for a port
 app.listen(3000);
 
+//Setting up the DB
 import { MongoClient } from "mongodb";
 import assert from "assert";
 import { SiteKeys } from "./site_keys";
 
-// Connection URL
-const url = "mongodb://localhost:27017";
-
 // Create a new MongoClient
-const client = new MongoClient(url, { useUnifiedTopology: true });
+const dbClient: MongoClient = new MongoClient(SiteKeys.dbURL, {
+	useUnifiedTopology: true,
+});
 
 // Use connect method to connect to the Server
-client.connect(function (err) {
+dbClient.connect(function (err) {
 	assert.equal(null, err);
 	console.log("Connected successfully to server");
 	console.log(SiteKeys.dbName);
 
 	const db = client.db(SiteKeys.dbName);
 
-	client.close();
+	dbClient.close();
 });
