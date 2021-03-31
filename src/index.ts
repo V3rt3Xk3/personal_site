da531dfcc75c552e6f293961eba68ab7e8612d65
V3rt3Xk3 <zoltanbalazsacademic@gmail.com>
2021-03-31 22:59:20 +0200

    // i have tested teh mongoDB connection and it works

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,25 @@ app.get("/", (_request, _response) => {
 
 // Listening for a port
 app.listen(3000);
+
+import { MongoClient } from "mongodb";
+import assert from "assert";
+
+// Connection URL
+const url = "mongodb://localhost:27017";
+
+// Database Name
+const dbName = "personal_site";
+
+// Create a new MongoClient
+const client = new MongoClient(url, { useUnifiedTopology: true });
+
+// Use connect method to connect to the Server
+client.connect(function (err) {
+	assert.equal(null, err);
+	console.log("Connected successfully to server");
+
+	const db = client.db(dbName);
+
+	client.close();
+});
