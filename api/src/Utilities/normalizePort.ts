9cd5f9b169908034fb00869e21c603fae053e30a
V3rt3Xk3 <zoltanbalazsacademic@gmail.com>
2021-04-01 21:56:21 +0200

    // i have gotten a normalizePort function from the web, to use for port setting

---
+++ api/src/Utilities/normalizePort.ts
@@ -0,0 +1,17 @@
+// Saved this function from stackoverflow
+
+export function normalizePort(val: any) {
+	var port = parseInt(val, 10);
+
+	if (isNaN(port)) {
+		// named pipe
+		return val;
+	}
+
+	if (port >= 0) {
+		// port number
+		return port;
+	}
+
+	return false;
+}
