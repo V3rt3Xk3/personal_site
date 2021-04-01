05c0832a437ad1ab7d1383635869f67033450a08
V3rt3Xk3 <zoltanbalazsacademic@gmail.com>
2021-04-01 15:49:15 +0200

    // started to figure out some data models with mongoose's schema

---
+++ src/DataModels/BlogPostModel.ts
@@ -0,0 +1,16 @@
+import Mongoose from "mongoose";
+
+const BlogPostSchema = new Mongoose.Schema({
+	title: {
+		type: String,
+		required: true,
+	},
+	content: {
+		type: String,
+		required: true,
+	},
+	date: {
+		type: Date,
+		default: Date.now,
+	},
+});
