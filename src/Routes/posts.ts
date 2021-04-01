66e0b8622f2f0831d00070623fd7b41edc941fe5
V3rt3Xk3 <zoltanbalazsacademic@gmail.com>
2021-04-01 19:03:53 +0200

    //this seems to be the way the post request has to be managed

--- src/Routes/posts.ts
+++ src/Routes/posts.ts
@@ -1,10 +1,25 @@
 import express, { Router } from "express";
+import BlogPost from "../DataModels/BlogPostModel";
 
 const postsRoute: Router = express.Router();
 
 // Routes
 postsRoute.post("/blog_insert", (_request, _response) => {
 	console.log(_request.body);
+
+	const post = new BlogPost({
+		title: _request.body.title,
+		content: _request.body.content,
+	});
+
+	post
+		.save()
+		.then((data) => {
+			_response.json(data);
+		})
+		.catch((error) => {
+			_response.status(200);
+		});
 });
 postsRoute.post("/special", (_request, _response) => {
 	console.log(_request.body);
