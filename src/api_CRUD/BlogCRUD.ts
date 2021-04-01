171ab7641ea4446829a3f43e7e34a411416db414
V3rt3Xk3 <zoltanbalazsacademic@gmail.com>
2021-04-01 19:59:11 +0200

    // restructured the CRUD methods for the blog posts

---
+++ src/api_CRUD/BlogCRUD.ts
@@ -0,0 +1,70 @@
+import express, { Router } from "express";
+import BlogPost from "../DataModels/BlogPostModel";
+
+const BlogCRUD: Router = express.Router();
+
+// Blog GETS
+BlogCRUD.get("/blogs", async (_request, _response) => {
+	try {
+		const blogEntry = await BlogPost.find();
+		_response.json(blogEntry);
+	} catch (_error) {
+		_response.json({ message: _error });
+	}
+});
+
+BlogCRUD.get("/blogbyid/:blogId", async (_request, _response) => {
+	try {
+		const blogEntry = await BlogPost.findById(_request.params.blogId);
+		_response.json(blogEntry);
+	} catch (_error) {
+		_response.json({ message: _error });
+	}
+});
+
+// Blog POSTS
+
+BlogCRUD.post("/blog_insert", async (_request, _response) => {
+	console.log(_request.body);
+
+	const post = new BlogPost({
+		title: _request.body.title,
+		content: _request.body.content,
+	});
+
+	try {
+		const savedPost = await post.save();
+		_response.json(savedPost);
+	} catch (_error) {
+		_response.json({ message: _error });
+	}
+});
+
+// blog UPDATES
+BlogCRUD.patch("/updatebyblogid/:blogId", async (_request, _response) => {
+	try {
+		const updateBlogEntry = await BlogPost.updateOne(
+			{
+				_id: _request.params.blogId,
+			},
+			{ $set: { title: _request.body.title, content: _request.body.content } }
+		);
+		_response.json(updateBlogEntry);
+	} catch (_error) {
+		_response.json({ message: _error });
+	}
+});
+
+// blog DELETES
+BlogCRUD.delete("/deleteblogbyid/:blogId", async (_request, _response) => {
+	try {
+		const removedBlogEntry = await BlogPost.remove({
+			_id: _request.params.blogId,
+		});
+		_response.json(removedBlogEntry);
+	} catch (_error) {
+		_response.json({ message: _error });
+	}
+});
+
+module.exports = BlogCRUD;
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 //Setting up express
 import express from "express";
-const getsRoute = require("./Routes/gets");
-const postsRoute = require("./Routes/posts");
-const deletesRoute = require("./Routes/deletes");
+const BlogCRUD = require("./api_CRUD/BlogCRUD");
 //Setting up the DB
 import Mongoose from "mongoose";
 import { SiteKeys } from "./site_keys";
@@ -27,13 +25,7 @@ const app = express();
 //Route MIddlewares
 //Posts
 // i am unsure whether this is the best way to do it
-app.use("/posts", express.json({ limit: "1mb" }));
-app.use("/posts", postsRoute);
-
-//Deletes
-app.use("/deletes", deletesRoute);
-
-//Gets
-app.use("/gets", getsRoute);
+app.use("/blogmethods", express.json({ limit: "1mb" }));
+app.use("/blogmethods", BlogCRUD);
 // Listening for a port
 app.listen(3000);
