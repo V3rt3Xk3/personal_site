03484c4291eb4221c4f882fd54c72cc2fd54db17
V3rt3Xk3 <zoltanbalazsacademic@gmail.com>
2021-04-02 15:46:45 +0200

    I have written my very first, very real almost alright TDD test

--- api/Tests/Blogs/blogs.test.ts
+++ api/Tests/Blogs/blogs.test.ts
@@ -16,68 +16,88 @@ const PORT = normalizePort(process.env.PORT || "9000");
 const HOST = `http://localhost:${PORT}`;
 
 describe("api", () => {
-	// before((done) => {
-	// 	dbHandler.dbConnect().then(() => {
-	// 		return done();
-	// 	});
-	// });
-	beforeEach((done) => {
-		Mongoose.connection.collections["blogposts"].drop().then(
-			() => {
-				// console.log("Dropped blogposts collection");
-				const rawTestBlogData = fs.readFileSync(
-					"./Tests/Blogs/testBlogData.json",
-					"utf8"
-				);
-				const testBlogData = JSON.parse(rawTestBlogData);
-				BlogPost.insertMany(testBlogData.BlogPosts).then(
-					() => {
-						// console.log("Inserted 2 documents");
-						done();
-					},
-					(_error) => {
-						throw _error;
-					}
-				);
-			},
-			(_error) => {
-				throw _error;
+	describe("BlogMethods API", () => {
+		beforeEach((done) => {
+			Mongoose.connection.collections["blogposts"].drop().then(
+				() => {
+					// console.log("Dropped blogposts collection");
+					const rawTestBlogData = fs.readFileSync(
+						"./Tests/Blogs/testBlogData.json",
+						"utf8"
+					);
+					const testBlogData = JSON.parse(rawTestBlogData);
+					BlogPost.insertMany(testBlogData.BlogPosts).then(
+						() => {
+							// console.log("Inserted 2 documents");
+							done();
+						},
+						(_error) => {
+							throw _error;
+						}
+					);
+				},
+				(_error) => {
+					throw _error;
+				}
+			);
+		});
+
+		after((done) => {
+			dbHandler.dbClose();
+			done();
+		});
+
+		it("/blogmethods/bloginsertone - Should accept valid data", (done) => {
+			const goodRequest = {
+				title: "TEST TITLE",
+				content: "TEST CONTENT",
+			};
+			request(HOST)
+				.post("/blogmethods/bloginsertone")
+				.send(goodRequest)
+				.expect((_response) => {
+					const responseBody = _response.body;
+					expect(responseBody).to.contain.property("_id");
+					expect(responseBody).to.contain.property("title");
+					expect(responseBody).to.contain.property("content");
+					expect(responseBody).to.contain.property("date");
+				})
+				.expect(200, done);
+		});
+
+		it("/blogmethods/blogs - Should retrieve the number of records in mock DB", (done) => {
+			request(HOST)
+				.get("/blogmethods/blogs")
+				.expect((_response) => {
+					const responseBody = _response.body;
+					expect(responseBody).to.have.lengthOf(3);
+					// This one test whether there is an element in the array, with TEST TITLE
+					expect(responseBody.some(Array, { title: "TITLE2" })).to.be.true;
+				})
+				.expect(200, done);
+		});
+		it(
+			"/blogmethods/deleteblogbytitle/:blogTitle " +
+				"- Should delete an element in mock DB (Title: 'TITLE2'",
+			(done) => {
+				request(HOST)
+					.delete("/blogmethods/deleteblogbytitle/TITLE2")
+					.expect(() => {
+						BlogPost.countDocuments({}, (_error, _count) => {})
+							.then((_count) => {
+								expect(_count).to.equal(2);
+							})
+							.then(() => {
+								BlogPost.find().then((_queryResult) => {
+									expect(_queryResult).some(Array, { title: "TITLE2" }).to.be
+										.false;
+								});
+							});
+
+						// This one test whether there is an element in the array, with TEST TITLE
+					})
+					.expect(200, done);
 			}
 		);
-	});
-
-	after((done) => {
-		dbHandler.dbClose();
-		done();
-	});
-
-	it("Should accept valid data", (done) => {
-		const goodRequest = {
-			title: "TEST TITLE",
-			content: "TEST CONTENT",
-		};
-		request(HOST)
-			.post("/blogmethods/bloginsertone")
-			.send(goodRequest)
-			.expect((_response) => {
-				const responseBody = _response.body;
-				expect(responseBody).to.contain.property("_id");
-				expect(responseBody).to.contain.property("title");
-				expect(responseBody).to.contain.property("content");
-				expect(responseBody).to.contain.property("date");
-			})
-			.expect(200, done);
-	});
-
-	it("Should retrieve 1 document", (done) => {
-		request(HOST)
-			.get("/blogmethods/blogs")
-			.expect((_response) => {
-				const responseBody = _response.body;
-				expect(responseBody).to.have.lengthOf(2);
-				// This one test whether there is an element in the array, with TEST TITLE
-				expect(responseBody.some(Array, { title: "TITLE2" })).to.be.true;
-			})
-			.expect(200, done);
 	});
 });
